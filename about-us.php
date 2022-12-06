bb0975246939833140a1c27704b17614698dcefe
Pinku556 <baljeetpinku@gmail.com>
2022-12-06 01:04:06 -0500

    Handled Accessbility for addnewvehicle vehiclechecker

--- about-us.php
+++ about-us.php
@@ -36,7 +36,13 @@
                         <nav id="primary-nav" class="dropdown cf">
                             <ul class="dropdown menu">
                                 <li><a href="index.php">Home</a></li>
-                                <li><a href="vinreport.php">VIN Checker</a></li>
+                                <li class="dropdown">
+                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Checker<span class="caret"></span></a>
+                                    <ul class="dropdown-menu">
+                                    <li><a href="vinreport.php">VIN Checker</a></li>
+                                    <li><a href="vehiclechecker.php">Vehicle Image Checker</a></li>
+                                    </ul>
+                                </li>
                                 <li class='active'>
                                     <a href="#">About</a>
                                 </li>
